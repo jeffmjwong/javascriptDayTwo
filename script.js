bfd36c71c882bccd5fa820a06bbbcdc58b23596d
Jeff Wong <jeffmjwong@gmail.com>
2018-05-15 14:03:57 +1000

    ES5 prototypal inheritance vs ES6 class syntatic sugar

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// function Dog(name, location) {
+//   this.name = name;
+//   this.location = location;
+// }
+//
+// Dog.prototype.speak = function() {
+//   console.log(`Woof! My name is ${this.name}`);
+//   console.log(`My location is ${this.location}`);
+// }
+//
+// Dog.prototype.setLocation = function(location) {
+//   this.location = location;
+// }
 
+class Dog {
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
+
+  speak() {
+    console.log(`Woof! My name is ${this.name}`);
+    console.log(`My location is ${this.location}`);
+  }
+
+  setLocation(location) {
+    this.location = location;
+  }
+}
